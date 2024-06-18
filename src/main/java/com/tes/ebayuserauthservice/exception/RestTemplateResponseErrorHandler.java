81229defc1d0637ebdef263b8bbf06b45faf1229
exkernel <dev.exkernel@gmail.com>
2024-06-18 20:14:15 +0400

    refactor: wrap ExpiredRefreshTokenException in a RuntimeException class, because in this context, it's not something to recover from

--- src/main/java/com/tes/ebayuserauthservice/exception/RestTemplateResponseErrorHandler.java
+++ src/main/java/com/tes/ebayuserauthservice/exception/RestTemplateResponseErrorHandler.java
@@ -1,28 +1,29 @@
 package com.tes.ebayuserauthservice.exception;
 
-import com.tes.ebayuserauthservice.service.UserAuthCodeService;
-import com.tes.ebayuserauthservice.service.UserRefreshTokenService;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import com.tes.ebayuserauthservice.service.AuthCodeService;
+import com.tes.ebayuserauthservice.service.GenericRefreshTokenService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResponseErrorHandler;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Component
 public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
-    private UserAuthCodeService authCodeService;
-    private UserRefreshTokenService refreshTokenService;
+    private AuthCodeService authCodeService;
+    private GenericRefreshTokenService genericRefreshTokenService;
 
-    public RestTemplateResponseErrorHandler(UserAuthCodeService authCodeService) {
+    public RestTemplateResponseErrorHandler(AuthCodeService authCodeService) {
         this.authCodeService = authCodeService;
     }
 
-    public RestTemplateResponseErrorHandler(UserRefreshTokenService refreshTokenService) {
-        this.refreshTokenService = refreshTokenService;
+    public RestTemplateResponseErrorHandler(GenericRefreshTokenService genericRefreshTokenService) {
+        this.genericRefreshTokenService = genericRefreshTokenService;
     }
 
     @Override
@@ -41,15 +42,20 @@ public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
             // if it contains "refresh", it's the error
             // related to an expired or invalid refresh token.
             if (responseBodyText.contains("code")) {
-                throw new
-                        ExpiredAuthCodeException("The authorization code has expired. "
-                        + "It was valid for: "
-                        + authCodeService.findNewest().getExpiresIn() + " seconds");
+                throw new ExpiredAuthCodeException(
+                        "The authorization code has expired. "
+                                + "It was valid for: "
+                                + authCodeService.findLatest().getExpiresIn()
+                                + " seconds");
             } else if (responseBodyText.contains("refresh")) {
-                throw new
-                        ExpiredRefreshTokenException("The refresh token has expired. "
-                        + "It was valid for: "
-                        + refreshTokenService.findNewest().getExpiresIn() + " seconds");
+                throw new RuntimeException(
+                        new ExpiredRefreshTokenException(
+                                "The refresh token has expired. "
+                                        + "It was valid for: "
+                                        + genericRefreshTokenService.findLatest().getExpiresIn()
+                                        + " seconds"
+                        )
+                );
             }
         }
     }
